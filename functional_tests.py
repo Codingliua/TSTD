36b010254aef54f2b719fd8b16a822c0747e79ef
CodingLiu <16567739+codingliu12@user.noreply.gitee.com>
2026-05-07 11:56:36 +0800

    message5: The first iterative development

--- functional_tests.py
+++ functional_tests.py
@@ -2,7 +2,7 @@ from selenium import webdriver
 import unittest
 class NewVisitorTest(unittest.TestCase):
     def setUp(self):
-        self.browser = werbdriver.Chrome()
+        self.browser = webdriver.Chrome()
     def tearDown(self):
         self.browser.quit()
     def test_cana_start_a_list_and_retrieve_it_later(self):
