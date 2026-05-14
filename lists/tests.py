e8850b37f964fd96f47da5770ecd11704d122c57
CodingLiu <16567739+codingliu12@user.noreply.gitee.com>
2026-05-14 17:09:32 +0800

    message 17: change models for multiple users

--- lists/tests.py
+++ lists/tests.py
@@ -39,16 +39,22 @@ class HomePageTest(TestCase):
    
 class ListViewTest(TestCase):
     def test_uses_list_template(self):
-        response = self.client.get('/lists/the-new-page/')
-        self.assertTemplateUsed(response,'list.html')
-    def test_displays_all_list_items(self):
         list_user = List.objects.create()
-        Item.objects.create(text='itemey 1', list=list_user)
-        Item.objects.create(text='itemey 2', list=list_user)
-        response = self.client.get('/lists/the-new-page/')
+        response = self.client.get(f'/lists/{list_user.id}/')
+        self.assertTemplateUsed(response,'list.html')
+    def test_displays_only_items_for_that_list(self):
+        correct_list = List.objects.create()
+        Item.objects.create(text='itemey 1', list=correct_list)
+        Item.objects.create(text='itemey 2', list=correct_list)
+        other_list = List.objects.create()
+        Item.objects.create(text='other list item 1', list=other_list)
+        Item.objects.create(text='other list item 2', list=other_list)
+        response = self.client.get(f'/lists/{correct_list.id}/')
 
         self.assertContains(response,'itemey 1')
         self.assertContains(response,'itemey 2')
+        self.assertNotContains(response,'other list item 1')
+        self.assertNotContains(response,'other list item 2')
 class NewListTest(TestCase):
     def test_can_save_a_POST_request(self):
         response = self.client.post('/lists/new', data={'item_text':'A new list item'})
