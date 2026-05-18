f779bd3ec0855a1d912ae9acb3473486abfa6d9f
CodingLiu <16567739+codingliu12@user.noreply.gitee.com>
2026-05-18 15:18:39 +0800

    message 18: new URL and view for adding to existing lists. UT and FT pass

--- lists/tests.py
+++ lists/tests.py
@@ -38,6 +38,11 @@ class HomePageTest(TestCase):
         self.assertTemplateUsed(response,'home.html')
    
 class ListViewTest(TestCase):
+    def test_passes_correct_list_to_template(self):
+        other_list = List.objects.create()
+        correct_list = List.objects.create()
+        response = self.client.get(f'/lists/{correct_list.id}/')
+        self.assertEqual(response.context['list'],correct_list)
     def test_uses_list_template(self):
         list_user = List.objects.create()
         response = self.client.get(f'/lists/{list_user.id}/')
@@ -65,6 +70,24 @@ class NewListTest(TestCase):
 
     def test_redirects_after_POST(self):
         response = self.client.post('/lists/new', data={'item_text':'A new list item'})
-        self.assertRedirects(response,'/lists/the-new-page/')
+        new_list = List.objects.first()
+        self.assertRedirects(response,f'/lists/{new_list.id}/')
+class NewItemTest(TestCase):
+    def test_can_save_a_POST_request_to_an_existing_list(self):
+        other_list = List.objects.create()
+        correct_list = List.objects.create()
+
+        self.client.post(f'/lists/{correct_list.id}/add_item', data={'item_text':'A new item for an existing list'})
+
+        self.assertEqual(Item.objects.count(),1)
+        new_item = Item.objects.first()
+        self.assertEqual(new_item.text,'A new item for an existing list')
+        self.assertEqual(new_item.list,correct_list)
+    def test_redirects_to_list_view(self):
+        other_list = List.objects.create()
+        correct_list = List.objects.create()
+
+        response = self.client.post(f'/lists/{correct_list.id}/add_item', data={'item_text':'A new item for an existing list'})
+        self.assertRedirects(response,f'/lists/{correct_list.id}/')
 
 # Create your tests here.
